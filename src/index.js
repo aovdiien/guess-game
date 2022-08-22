50accb012f273afa20ef1b99bbd07538fbcf9945
Andrii Ovdiienko <Andrii.Ovdiienko@ibm.com>
2022-08-22 10:14:21 +0300

    feat: update tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import { store } from './store';
-import './styles/index.scss';
 import App from './components/App';
+
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
